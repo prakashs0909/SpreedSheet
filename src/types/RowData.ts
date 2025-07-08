e5846f82c27f5b1c0552e50864677e546202c0d2
prakash <ps8780909@gmail.com>
2025-07-08 12:48:11 +0530

    add Rowdata type definition

---
+++ src/types/RowData.ts
@@ -0,0 +1,11 @@
+export type RowData = {
+  job: string;
+  submitted: string;
+  status: string;
+  submitter: string;
+  url: string;
+  assigned: string;
+  priority: string;
+  due: string;
+  value: string;
+};
